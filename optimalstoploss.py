81512e3cd0c4ea719e63b13e6195ed6463bbf191
shamchittesh <shamchittesh@gmail.com>
2024-10-12 10:05:04 +0400

    working optimal stop loss

--- optimalstoploss.py
+++ optimalstoploss.py
@@ -1,9 +1,9 @@
 from modules import *
 
-start_date  = "1999-08-11"
-end_date    = "2003-08-11"
+start_date  = "1999-08-15"
+end_date    = "2024-08-14"
 stock       = "QQQ"
-TPT         = 2
+TPT         = 2 #Time of trade AKA no of days trading session active else take closing price of nth day
 
 csv_loc = f'./data/{stock}.csv'
 df = pd.read_csv(csv_loc, sep=',', header=0)
@@ -42,30 +42,41 @@ for i in range(0, len(df), TPT):
         # Append the new row to the new DataFrame using pd.concat
         new_df = pd.concat([new_df, pd.DataFrame([new_row])], ignore_index=True)
 
+# Calculate statistics on column D
+average_D = new_df['D'].mean()
+smallest_D = new_df['D'].min()
+highest_D = new_df['D'].max()
+
 print(new_df)
+print(f"\nAverage of D: {average_D}")
+print(f"Smallest value of D: {smallest_D}")
+print(f"Highest value of D: {highest_D}")
+
+finding_nemo = []
+
+for X in np.arange(2.6, 3.2, 0.01):
+    # Calculate the new column 'tx' based on the value of X
+    count = 0
+    new_df['tx'] = new_df.apply(lambda row: row['P'] if X > row['D'] else -X, axis=1)    # Calculate the new column 'tx' based on the value of X
+    # Count the number of times X < row['D']
+    for _, row in new_df.iterrows():
+        if X < row['D']:
+            count += 1
+    new_df['balance_tx'] = new_df['tx'].cumsum()
+    last_balance_tx = new_df['balance_tx'].iloc[-1]
+    finding_nemo.append({'X': X, 'sum_balance_tx': last_balance_tx, 'SL_hits': count})
+
+print(new_df)
+finding_nemo = pd.DataFrame(finding_nemo)
+
+# Finding the highest value of last_balance_tx and its corresponding X
+max_index = finding_nemo['sum_balance_tx'].idxmax()  # Index of the max balance_tx
+max_value = finding_nemo['sum_balance_tx'].max()     # Maximum balance_tx value
+corresponding_X = finding_nemo.loc[max_index, 'X']    # Corresponding X value
+SL_hits = finding_nemo.loc[max_index, 'SL_hits']  # Corresponding Count X < D
+
+# Print the result
+print(f"\nHighest Sum balance_tx: {max_value:.4f} at X = {corresponding_X:.4f} with SL Hits = {SL_hits}")
 
 
-
-# Columns:
-# Trade number
-# X stop loss
-# D (absolute lowest price)
-# P (Closing price)
-# Balance (Culmulative P)
-# Tx (if x > D, P, -x)
-# Balance Tx culmulative tx
-
-# Further upgrades: P = 2X
-# Trading Time in days (start with default intra day trading to decide closing price)
-
-# Inputs: df, starting amount=0, tpt=1, rrr(P)=closing price, 
-# outputs: X, final amount, trade number, 
-
-# Process:
-# current dataframe:
-# Date,Open,High,Low,Close,Adj Close,Volume
-
-# D=Low, 
-
-# transformed dataframe:
-# Date(TPT),Open(TPT),High(TPT),D=Low(TPT),P=Close(TPT),tx,balance_tx
+print(finding_nemo)
